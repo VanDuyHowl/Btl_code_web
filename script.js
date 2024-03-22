8fe08882cefd94e4dc89866487059e22dbf95f85
VanDuyHowl <160881058+VanDuyHowl@users.noreply.github.com>
2024-03-22 22:31:26 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,10 @@
 /*Đăng ký */
- $(document).ready(function(){
-     $("#js-Dangky").click(function(){
-         alert("Đã click vào đăng ký");
-         $("#js-modal").show();
-     });
- });
+$(document).ready(function(){
+    $("#js-Dangky").click(function(){
+        $("#js-modal").css("display","flex");
 
-// /* */
- $(document).ready(function(){
-     $("#js-back").click(function(){
-         alert("Đã quay lại trang chủ");
-         $("#js-modal").hide();
-     });
- });
-
-
-// const DangKy = document.querySelector('#js-Dangky')
-// const modal = document.querySelector('#js-moadal')
-// const Back = document.querySelector('#js-back')
-
-// DangKy.addEventListener('click', showDangKy)
-// Back.addEventListener('click',hideDangky)
-
-// function showDangKy(){
-//     modal.classList.add('open')
-// }
-// function hideDangky(){
-//     modal.classList.remove('open')
-// }
+        $("#js-back").click(function(){
+            $("#js-modal").css("display","none");
+        });
+    });
+});
